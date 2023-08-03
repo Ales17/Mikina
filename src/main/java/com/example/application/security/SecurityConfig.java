20af3f43ccd1d2d4be53448bea3e66adf8bc938a
Ales Bacul <alesbacul@gmail.com>
2023-08-03 18:35:18 +0200

    New JAR

--- src/main/java/com/example/application/security/SecurityConfig.java
+++ src/main/java/com/example/application/security/SecurityConfig.java
@@ -26,11 +26,9 @@ public class SecurityConfig extends VaadinWebSecurity {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeHttpRequests()
-//                .requestMatchers("/images/*.png").permitAll();
-        http.authorizeHttpRequests(auth -> auth
-                .requestMatchers("/images/*.png").permitAll()
-                .anyRequest().authenticated());
+ http.authorizeHttpRequests()
+               .requestMatchers("/images/*.png").permitAll();
+
         super.configure(http);
         setLoginView(http, LoginView.class);
     }
