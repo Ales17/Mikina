2ffc5a06b5a39f0826f16f57b92d3f18e92a341c
Ales Bacul <alesbacul@gmail.com>
2024-01-20 18:41:13 +0100

    lombok implemented

--- src/main/java/cz/ales17/mikina/data/entity/Guest.java
+++ src/main/java/cz/ales17/mikina/data/entity/Guest.java
@@ -7,9 +7,13 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "guest")
 public class Guest extends AbstractEntity {
@@ -27,79 +31,16 @@ public class Guest extends AbstractEntity {
     private LocalDate dateLeft;
 
     private String address;
+
     @Enumerated(EnumType.ORDINAL)
     private StatniPrislusnost nationality;
 
     private String idNumber;
 
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public LocalDate getDateArrived() {
-        return dateArrived;
-    }
-
-    public void setDateArrived(LocalDate dateArrived) {
-        this.dateArrived = dateArrived;
-    }
-
-    public LocalDate getDateLeft() {
-        return dateLeft;
-    }
-
-    public void setDateLeft(LocalDate dateLeft) {
-        this.dateLeft = dateLeft;
-    }
-
     @Override
     public String toString() {
         return firstName + " " + lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getIdNumber() {
-        return idNumber;
-    }
-
-    public void setIdNumber(String idNumber) {
-        this.idNumber = idNumber;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public StatniPrislusnost getNationality() {
-        return nationality;
-    }
-
-    public void setNationality(StatniPrislusnost nationality) {
-        this.nationality = nationality;
     }
 
 
--- src/main/java/cz/ales17/mikina/data/entity/Status.java
+++ src/main/java/cz/ales17/mikina/data/entity/Status.java
@@ -1,20 +1,16 @@
 package cz.ales17.mikina.data.entity;
 
 import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 public class Status extends AbstractEntity {
     private String name;
 
     public Status() {
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
 }
