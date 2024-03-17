e494fed59a49edac925d8fa07740929ee513b5b5
Ales Bacul <alesbacul@gmail.com>
2024-03-17 12:29:37 +0100

    company entity column lengts

--- src/main/java/cz/ales17/mikina/data/model/Company.java
+++ src/main/java/cz/ales17/mikina/data/model/Company.java
@@ -1,26 +1,52 @@
 package cz.ales17.mikina.data.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
 
+// Column lengths are set to be compatible with UNL (Ubyport)
 @Getter
 @Setter
 @Entity
 public class Company extends AbstractEntity {
+    @Column(length = 14)
+    private String ubyportId;
+
+    @Column(length = 5)
+    private String ubyportAbbr;
+
+    @Column(length = 35)
     private String name;
-    private String street;
-    // Cislo domovni - house number
-    private String houseNumber;
-    // Cislo evidencni - registration number
-    private String registrationNumber;
-    private String zipCode;
+
+    @Column(length = 50)
+    private String ubyportContact;
+
+    @Column(length = 32)
     private String district;
+
+    @Column(length = 48)
+    private String municipality;
+
+    @Column(length = 48)
+    private String municipalityQuarter;
+
+    @Column(length = 48)
+    private String street;
+
+    // Cislo domovni - house number
+    @Column(length = 5)
+    private String houseNumber;
+
+    // Cislo evidencni - registration number
+    @Column(length = 4)
+    private String registrationNumber;
+
+    @Column(length = 5)
+    private String zipCode;
+
     // Ubytovaci poplatek - stay fee per day (except the first one)
     private double stayFee;
-    private String municipality;
-    private String municipalityQuarter;
-    private String ubyportId;
-    private String ubyportAbbr;
-    private String ubyportContact;
+
+
 }
