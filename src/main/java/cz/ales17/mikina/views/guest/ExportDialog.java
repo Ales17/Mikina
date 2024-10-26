2daa3d4ed414f202b05cdd3160c579a659413ee6
Ales Bacul <alesbacul@gmail.com>
2024-10-26 11:03:35 +0200

    redesign export dialog

--- src/main/java/cz/ales17/mikina/views/guest/ExportDialog.java
+++ src/main/java/cz/ales17/mikina/views/guest/ExportDialog.java
@@ -1,26 +1,42 @@
 package cz.ales17.mikina.views.guest;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 public class ExportDialog extends Dialog {
-    final Anchor pdfBtn = new Anchor("#");
-    final Anchor unlBtn = new Anchor("#");
+    final Anchor pdfAnchor = new Anchor("#");
+    final Anchor unlAnchor = new Anchor("#");
     final Button close = new Button("Storno", e -> close());
 
 
     public ExportDialog() {
         setHeaderTitle("Exportovat");
-        pdfBtn.add(new Button("PDF", new Icon(VaadinIcon.DOWNLOAD_ALT)));
-        pdfBtn.setEnabled(false);
-        unlBtn.add(new Button("UNL (Ubyport)", new Icon(VaadinIcon.DOWNLOAD_ALT)));
-        unlBtn.setEnabled(false);
+        HorizontalLayout dialogText = new HorizontalLayout();
+        Paragraph text = new Paragraph("Reporty byly uloženy do sekce Historie reportů");
+        dialogText.add(text);
+        Button pdfBtn = new Button("PDF", new Icon(VaadinIcon.DOWNLOAD_ALT));
+        pdfBtn.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        Button unlBtn = new Button("UNL (Ubyport)", new Icon(VaadinIcon.DOWNLOAD_ALT));
+        unlBtn.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        pdfAnchor.add(pdfBtn);
+        pdfAnchor.setEnabled(false);
+
+        unlAnchor.add(unlBtn);
+        unlAnchor.setEnabled(false);
+
         HorizontalLayout dialogToolbar = new HorizontalLayout();
-        dialogToolbar.add(pdfBtn, unlBtn, close);
-        add(dialogToolbar);
+        dialogToolbar.add(pdfAnchor, unlAnchor, close);
+
+        VerticalLayout verticalLayout = new VerticalLayout(dialogText, dialogToolbar);
+        add(verticalLayout);
     }
 }
